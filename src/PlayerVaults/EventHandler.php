33cb35b78bc072e3335cb687757b38b34b1cb822
Muqsit Rayyan <Muqsit@users.noreply.github.com>
2017-04-28 16:29:39 +0300

    Licensing

--- src/PlayerVaults/EventHandler.php
+++ src/PlayerVaults/EventHandler.php
@@ -1,4 +1,26 @@
 <?php
+/*
+*
+* Copyright (C) 2017 Muqsit Rayyan
+*
+*    ___ _                                        _ _       
+*   / _ \ | __ _ _   _  ___ _ __/\   /\__ _ _   _| | |_ ___ 
+*  / /_)/ |/ _" | | | |/ _ \ "__\ \ / / _" | | | | | __/ __|
+* / ___/| | (_| | |_| |  __/ |   \ V / (_| | |_| | | |_\__ \
+* \/    |_|\__,_|\__, |\___|_|    \_/ \__,_|\__,_|_|\__|___/
+*                |___/                                      
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+*
+* @author Muqsit Rayyan
+* Twiter: http://twitter.com/muqsitrayyan
+* GitHub: http://github.com/Muqsit
+*
+*/
 namespace PlayerVaults;
 
 use pocketmine\event\inventory\InventoryTransactionEvent;
