48446f23c36511c8d44438e54caeefb7030951ff
Muqsit Rayyan <Muqsit@users.noreply.github.com>
2017-08-12 15:34:33 +0400

    Remove constructor (initial pmmp-1.2 update)

--- src/PlayerVaults/Vault/VaultInventory.php
+++ src/PlayerVaults/Vault/VaultInventory.php
@@ -30,10 +30,6 @@ use pocketmine\Player;
 
 class VaultInventory extends ChestInventory{
 
-    public function __construct(Vault $tile){
-        parent::__construct($tile, InventoryType::get(InventoryType::CHEST));
-    }
-
     public function onClose(Player $who){
         if(isset($this->getHolder()->namedtag->Vault)){
             PlayerVaults::getInstance()->getData()->saveContents($this->getHolder(), $this->getContents());
