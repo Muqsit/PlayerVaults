8c3090617219fae49ef5d30629987b04cd989943
maxxie114 <maxxie114@gmail.com>
2017-11-28 16:29:17 -0800

    Update API to 3.0.0-ALPHA9
    I don't know how to merge all changes into one pull request. These changes are being tested, except one exception about onCommand() is bool type, everything else works fine for 1.2.5 server.

--- src/PlayerVaults/Vault/VaultInventory.php
+++ src/PlayerVaults/Vault/VaultInventory.php
@@ -30,11 +30,16 @@ use pocketmine\Player;
 
 class VaultInventory extends ChestInventory{
 
-    public function onClose(Player $who): void{
+    public function __construct(Vault $tile){
+        parent::__construct($tile);
+    }
+
+    public function onClose(Player $who) : void{
         if(isset($this->getHolder()->namedtag->Vault)){
             PlayerVaults::getInstance()->getData()->saveContents($this->getHolder(), $this->getContents());
         }
         $this->holder->sendReplacement($who);
         $this->holder->close();
+        parent::onClose($who);
     }
 }
