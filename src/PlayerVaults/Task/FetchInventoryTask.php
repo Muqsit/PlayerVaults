53998033cbc2603dd35b8820a93b62d47101387c
BlockOfDevs <34788016+BlockOfDevs@users.noreply.github.com>
2018-01-14 05:26:31 +0100

    Update FetchInventoryTask.php

--- src/PlayerVaults/Task/FetchInventoryTask.php
+++ src/PlayerVaults/Task/FetchInventoryTask.php
@@ -26,7 +26,7 @@ namespace PlayerVaults\Task;
 use PlayerVaults\{PlayerVaults, Provider};
 
 use pocketmine\item\Item;
-use pocketmine\nbt\NBT;
+use pocketmine\nbt\NetworkLittleEndianNBTStream;
 use pocketmine\scheduler\AsyncTask;
 use pocketmine\Server;
 
@@ -93,7 +93,7 @@ class FetchInventoryTask extends AsyncTask{
         if(empty($data)){
             $this->setResult([]);
         }else{
-            $nbt = new NBT(NBT::BIG_ENDIAN);
+            $nbt = new NetworkLittleEndianNBTStream();
             $nbt->readCompressed($data);
             $nbt = $nbt->getData();
             $items = $nbt->ItemList ?? [];
